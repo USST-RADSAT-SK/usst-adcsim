e2b52260ddfcec32f44bda06717736644a3d4c0f
Curtis Puetz <cmp310@mail.usask.ca>
2019-03-15 20:23:08 -0600

    fix minor mistake

--- state_propagations.py
+++ state_propagations.py
@@ -16,7 +16,7 @@ def state_dot(state, control, inertia, inertia_inv):
 def state_dot_ref_frame(state, control, omega_r, inertia, inertia_inv):
     # sigmas
     a = (1 - state[0] @ state[0]) * np.identity(3) + 2 * ut.cross_product_operator(state[0]) + 2 * np.outer(state[0], state[0])
-    sigma_propagation = (1/4) * a  @ omega_r
+    sigma_propagation = (1/4) * a  @ (state[1] - omega_r)
 
     # omegas
     omega_propagation = inertia_inv @ ((-ut.cross_product_operator(state[1]) @ inertia @ state[1]) + control)
