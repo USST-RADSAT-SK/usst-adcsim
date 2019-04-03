b00b8eadee738116001ca62a13ecbb478c3b31d7
Taran Warnock <tww275@mail.usask.ca>
2019-04-02 22:49:44 -0600

    can find atmospheric mass density using NRLMSISE-00 model; added script which creates a netcdf file storing some of the required parameters

---
+++ atmospheric_density.py
@@ -0,0 +1,130 @@
+import xarray as xr
+import sys
+import os
+import numpy as np
+import datetime as dt
+from space_weather import create_space_weather_netcdf
+
+sys.path.insert(0, './Python-NRLMSISE-00/')
+from nrlmsise_00_header import *
+from nrlmsise_00 import *
+
+
+class AirDensityModel:
+    def __init__(self, path_to_space_weather_netcdf='./cssi_space_weather.nc'):
+        """
+        Parameters
+        ----------
+        path_to_space_weather_netcdf : str
+            file path of the space weather netcdf file, including the filename at the end
+            i.e. /path/to/file/cssi_space_weather.nc
+            By default, looks for the file in the current directory, and creates a new one if it can not be found.
+        """
+        if not os.path.isfile(path_to_space_weather_netcdf):
+            # create the space weather netcdf if it can't be found
+            create_space_weather_netcdf('http://celestrak.com/SpaceData/SW-Last5Years.txt',
+                                        path_to_space_weather_netcdf)
+        self._space_dataset = xr.open_dataset(path_to_space_weather_netcdf)
+
+    def air_density(self, year=0, doy=0, sec=0.0, alt=0.0, g_lat=0.0, g_long=0.0):
+        """
+        Parameters
+        ----------
+        year : int
+            year, currently ignored
+        doy
+            day of year
+        sec
+            seconds in day (UT)
+        alt
+            altitude in kilometers
+        g_lat
+            geodetic latitude
+        g_long
+            geodetic longitude
+
+        Returns
+        -------
+        float
+        """
+        date = self._get_date(year, doy)
+        # 81 day average of F10.7 flux (centered on doy)
+        f107A = self._space_dataset.ctr81_adj.sel(date=date)  # TODO: should we be using the adjusted or observed value?
+        # daily F10.7 flux for previous day
+        f107 = self._space_dataset.f107_adj.sel(date=date)  # adjusted or observed? should this be from the previous day?
+        # magnetic index(daily)
+        ap = self._space_dataset.ap_avg.sel(date=date)  # use average value from the day
+        # average magnetic index?
+        ap_a = ap  # is ap_a the average value?
+
+        output = nrlmsise_output()
+        # note: lst is the local apparent solar time
+        # I'm using the recommended formula in nrlmsise_00_header.py to calculate it
+        input = nrlmsise_input(year=year, doy=doy, sec=sec, alt=alt, g_lat=g_lat, g_long=g_long,
+                               lst=(sec/3600 + g_long/15), f107A=f107A, f107=f107, ap=ap, ap_a=ap_a)
+        flags = nrlmsise_flags()
+        # using the default recommended switches for now
+        flags.switches[0] = 0
+        for i in range(1, len(flags.switches)):
+            flags.switches[i] = 1
+
+        # call the model
+        gtd7(input, flags, output)
+
+        mass_density = output.d[5]  # total mass density in GM/CM3
+
+        return mass_density
+
+    def _get_date(self, year, doy):
+        """
+        Parameters
+        ----------
+        year : int
+            current year
+        doy : int
+            number of days into the year
+
+        Returns
+        -------
+        datetime
+        """
+        days_in_month = np.array([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+        if self._is_leap_year(year):
+            days_in_month[1] = 29
+
+        # get the date
+        month = 1
+        day_of_month = doy
+        day_counter = doy - days_in_month[0]
+        while day_counter > 0:
+            day_of_month = day_counter
+            day_counter = day_counter - days_in_month[month]  # month number and index are offset by one
+            month = month + 1
+        date = dt.datetime(year, month, day_of_month)
+
+        return date
+
+    @staticmethod
+    def _is_leap_year(year):
+        """
+        Taken from https://support.microsoft.com/en-ca/help/214019/method-to-determine-whether-a-year-is-a-leap-year
+
+        Parameters
+        ----------
+        year : int
+            current year
+
+        Returns
+        -------
+        bool
+        """
+        if year % 4 == 0:
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    return True
+                else:
+                    return False
+            else:
+                return True
+        else:
+            return False
---
+++ space_weather.py
@@ -0,0 +1,123 @@
+import numpy as np
+import xarray as xr
+import re
+import datetime as dt
+from urllib.request import urlopen
+
+url_last_five_years = 'http://celestrak.com/SpaceData/SW-Last5Years.txt'
+url_all = 'http://celestrak.com/SpaceData/SW-All.txt'
+
+
+def create_space_weather_netcdf(url_string='http://celestrak.com/SpaceData/SW-Last5Years.txt',
+                                output_name='cssi_space_weather.nc'):
+    # load the data
+    data = urlopen(url_string)
+    # create empty lists
+    date = []
+    bsrn = []  # Bartels Solar Rotation Number
+    nd = []  # number of days within Bartel 27-day cycle
+    kp = []
+    kp_sum = []
+    ap = []
+    ap_avg = []
+    cp = []
+    c9 = []
+    isn = []
+    f107_adj = []
+    q_flux = []
+    ctr81_adj = []  # adj -> adjusted to 1 AU
+    lst81_adj = []
+    f107_obs = []  # obs -> observed (unadjusted)
+    ctr81_obs = []
+    lst81_obs = []
+
+    while not (b'BEGIN OBSERVED' in data.readline()):
+        pass  # loops until we get to the line where data starts
+    line_byte = data.readline()  # grab the first line
+    while not (b'END OBSERVED' in line_byte):
+        line_str = line_byte.decode('utf-8')
+        values = re.findall(r'[\d.]+', line_str)
+        if not len(values) == 33:  # some lines near the end are missing values, skip them for now
+            # TODO: be smart and don't skip them because the f107 and ap values are still there
+            #   if we wanted to do this the lines would have to be read one character at a time
+            #   in order to locate the missing values
+            line_byte = data.readline()
+            continue
+        date.append(dt.datetime(int(values[0]), int(values[1]), int(values[2])))
+        bsrn.append(int(values[3]))
+        nd.append(int(values[4]))
+        kp.append(np.array([int(v) for v in values[5:13]]))
+        kp_sum.append(int(values[13]))
+        ap.append(np.array([int(v) for v in values[14:22]]))
+        ap_avg.append(int(values[22]))
+        cp.append(float(values[23]))
+        c9.append(int(values[24]))
+        isn.append(int(values[25]))
+        f107_adj.append(float(values[26]))
+        q_flux.append(int(values[27]))
+        ctr81_adj.append(float(values[28]))
+        lst81_adj.append(float(values[29]))
+        f107_obs.append(float(values[30]))
+        ctr81_obs.append(float(values[31]))
+        lst81_obs.append(float(values[32]))
+
+        line_byte = data.readline()  # read in next line
+
+    # TODO: add in predicted values, if desired
+    # while not (b'BEGIN DAILY PREDICTED' in data.readline()):
+    #     pass
+    # line_byte = data.readline()
+    # while not (b'END DAILY PREDICTED' in line_byte):
+    #
+    #     line_byte = data.readline()
+
+    data.close()
+    # convert to numpy arrays
+    # date = np.array(date)
+    # bsrn = np.array(bsrn)
+    # nd = np.array(nd)
+    # kp = np.array(kp)
+    # kp_sum = np.array(kp_sum)
+    # ap = np.array(ap)
+    # ap_avg = np.array(ap_avg)
+    # cp = np.array(cp)
+    # c9 = np.array(c9)
+    # isn = np.array(isn)
+    # f107_adj = np.array(f107_adj)
+    # q_flux = np.array(q_flux)
+    # ctr81_adj = np.array(ctr81_adj)
+    # lst81_adj = np.array(lst81_adj)
+    # f107_obs = np.array(f107_obs)
+    # ctr81_obs = np.array(ctr81_obs)
+    # lst81_obs = np.array(lst81_obs)
+    # three_hour_interval = np.array(['0000-0300', '0300-0600', '0600-0900', '0900-1200',
+    #                                 '1200-1500', '1500-1800', '1800-2100', '2100-0000'])
+    three_hour_interval = ['0000-0300', '0300-0600', '0600-0900', '0900-1200',
+                           '1200-1500', '1500-1800', '1800-2100', '2100-0000']
+
+    cssi_space_weather = xr.Dataset(
+        data_vars={'bsrn': ('date', bsrn),
+                   'nd': ('date', nd),
+                   'kp': (('date', 'three_hour_interval'), kp),
+                   'kp_sum': ('date', kp_sum),
+                   'ap': (('date', 'three_hour_interval'), ap),
+                   'ap_avg': ('date', ap_avg),
+                   'cp': ('date', cp),
+                   'c9': ('date', c9),
+                   'isn': ('date', isn),
+                   'f107_adj': ('date', f107_adj),
+                   'q_flux': ('date', q_flux),
+                   'ctr81_adj': ('date', ctr81_adj),
+                   'lst81_adj': ('date', lst81_adj),
+                   'f107_obs': ('date', f107_obs),
+                   'ctr81_ob': ('date', ctr81_obs),
+                   'lst81_obs': ('date', lst81_obs)},
+        coords={'date': date,
+                'three_hour_interval': three_hour_interval})
+
+    # make the netcdf file
+    cssi_space_weather.to_netcdf(output_name)
+
+
+if __name__ == '__main__':
+    create_space_weather_netcdf(url_last_five_years, 'cssi_space_weather.nc')
