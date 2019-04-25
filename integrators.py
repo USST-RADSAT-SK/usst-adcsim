fbd165fd214a8162b6bdfa151cdaebf59feb8572
Curtis Puetz <cmp310@mail.usask.ca>
2019-04-25 16:45:06 -0600

    minor documentation

--- integrators.py
+++ integrators.py
@@ -1,4 +1,8 @@
+# see https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods for rk4 implementation
+
+
 def rk4(fn, time_step, state, *args):
+    # *args is everything else that you would want to pass to 'fn'
     k1 = time_step*fn(state, *args)
     k2 = time_step*fn(state + k1/2, *args)
     k3 = time_step*fn(state + k2/2, *args)
@@ -7,6 +11,7 @@ def rk4(fn, time_step, state, *args):
 
 
 def rk4_general(fn, time_step, t, y, *args):
+    # *args is everything else that you would want to pass to 'fn'
     k1 = time_step*fn(t, y, *args)
     k2 = time_step*fn(t + time_step/2, y + k1/2, *args)
     k3 = time_step*fn(t + time_step/2, y + k2/2, *args)
