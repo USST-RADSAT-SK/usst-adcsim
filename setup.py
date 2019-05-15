14aad92d0e503b9ef0b1fa297621eb938dadfcba
Curtis Puetz <cmp310@mail.usask.ca>
2019-05-14 19:43:57 -0600

    fix setup file

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description='ADCS simulations for the USST CubeSat team',
     url='https://github.com/usst-adcs/U-of-Colorado_course',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    long_description=open('readme.md').read(),
 )
